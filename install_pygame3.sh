4ddb73d85d41219d7f46f0d2a1c1f55a47753323
David Youster <davidyouster@gmail.com>
2016-05-29 04:03:51 +0100

    Add pygame install script.

---
+++ install_pygame3.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install pygame for Python3 from source
+
+cd ~
+sudo apt-get install mercurial
+hg clone https://bitbucket.org/pygame/pygame
+cd pygame
+sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
+  libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev \
+  libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev
+python3 setup.py build && sudo python3 setup.py install
+
